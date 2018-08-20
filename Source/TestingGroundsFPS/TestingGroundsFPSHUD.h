9f2cfb79b4b354b7cdef5a1008d0af55ae3f011e
DESKTOP-VQ8PTVF\Ronald Goennier <rgoennier@gmail.com>
2018-08-20 15:20:34 -0700

    Initial commit

---
+++ Source/TestingGroundsFPS/TestingGroundsFPSHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TestingGroundsFPSHUD.generated.h"
+
+UCLASS()
+class ATestingGroundsFPSHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ATestingGroundsFPSHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
